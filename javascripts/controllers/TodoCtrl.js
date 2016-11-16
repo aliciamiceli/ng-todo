86fac7b20d89fcda9f393e6aa2ce8cb0c16cbcac
Alicia Miceli <akb.miceli@gmail.com>
2016-11-15 21:21:42 -0600

    add new item is working

--- javascripts/controllers/TodoCtrl.js
+++ javascripts/controllers/TodoCtrl.js
@@ -6,9 +6,15 @@ app.controller("TodoCtrl", function($scope, ItemFactory){
   $scope.newTask = {};
   $scope.items = [];
 
-ItemFactory.getItemList().then(function(fbItems){
-  console.log("items from controller", fbItems);
-});
+
+let getItems = function(){
+  ItemFactory.getItemList().then(function(fbItems){
+    $scope.items = fbItems;
+  })
+}
+
+getItems();
+
 
   $scope.allItems = function(){
     console.log("you clicked all items");
@@ -22,10 +28,13 @@ ItemFactory.getItemList().then(function(fbItems){
 
   $scope.addNewItem = function(){
     $scope.newTask.isCompleted = false;
-    $scope.newTask.id = $scope.items.length;
+    // $scope.newTask.id = $scope.items.length;
     console.log("newTask in function", $scope.newTask);
-    $scope.items.push($scope.newTask);
-    $scope.newTask = {};
-    $scope.showListView = true;
+    // $scope.items.push($scope.newTask);
+    ItemFactory.postNewItem($scope.newTask).then(function(itemId){
+      getItems();
+      $scope.newTask = {};
+      $scope.showListView = true;
+    });
   };
 });
